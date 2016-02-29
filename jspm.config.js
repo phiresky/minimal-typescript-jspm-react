0b7503309584dac7c581e97544f0886e817c147b
phiresky <phireskyde+git@gmail.com>
2016-02-29 02:16:05 +0100

    run `jspm install react react-dom`

--- jspm.config.js
+++ jspm.config.js
@@ -7,7 +7,10 @@ SystemJS.config({
   transpiler: "plugin-typescript",
 
   map: {
-    "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.1"
+    "plugin-typescript": "github:frankwallis/plugin-typescript@4.0.1",
+    "process": "github:jspm/nodelibs-process@0.2.0-alpha",
+    "react": "npm:react@0.14.7",
+    "react-dom": "npm:react-dom@0.14.7"
   },
 
   packages: {
@@ -15,6 +18,11 @@ SystemJS.config({
     "github:frankwallis/plugin-typescript@4.0.1": {
       "map": {
         "typescript": "npm:typescript@1.8.2"
+      }
+    },
+    "npm:react@0.14.7": {
+      "map": {
+        "fbjs": "npm:fbjs@0.6.1"
       }
     }
   }
